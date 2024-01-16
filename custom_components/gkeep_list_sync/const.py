e7a310acfddb3088c04df361c92ec48ca73d1866
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2024-01-16 16:00:31 -0500

    rebase: shorten service names (#16)

--- custom_components/gkeep_list_sync/const.py
+++ custom_components/gkeep_list_sync/const.py
@@ -7,4 +7,4 @@ CONF_BASE_USERNAME = 'base_username'
 DEFAULT_LIST_TITLE = "Groceries"
 MISSING_LIST = "missing_list"
 SHOPPING_LIST_DOMAIN = "shopping_list"
-SERVICE_NAME_BASE = "sync_list"
+SERVICE_NAME_BASE = "sync"
